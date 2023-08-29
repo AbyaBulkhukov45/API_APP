1217d6d7214384c9c8ee9d277b5881ea8aa76ced
PavelMinsky45 <Minsk5545@gmail.com>
2023-08-29 22:59:06 +0300

    v 3.0

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -57,7 +57,7 @@ class FollowListView(
     permission_classes = (permissions.IsAuthenticated, OwnerOnly)
 
     def get_queryset(self):
-        return Follow.objects.filter(user=self.request.user)
+        return self.request.user.followings.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
